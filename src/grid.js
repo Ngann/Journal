1798e339d7f3e60429c57d7a49f5564f4b9a0eda
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-01-07 21:46:53 -0800

    add input

--- src/grid.js
+++ src/grid.js
@@ -4,6 +4,7 @@ export function grid(el) {
     container.className = "container";
     var i = 0;
     var k = 0;
+    var j = 0;
     for (i=0; i<9; i+=1) {
         var row = document.createElement("div");
         row.className = "row";
@@ -13,6 +14,13 @@ export function grid(el) {
             var box = document.createElement("div");
             box.className = "box";
             row.appendChild(box);
+
+            for(j=0; j<1; j+=1){
+              var input = document.createElement("input");
+              input.className = "input";
+              input.type = "number";
+              box.appendChild(input);
+            }
         }
         container.appendChild(row);
     }
